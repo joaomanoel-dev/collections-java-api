8ab4e10b742fd6ccc4fc791d4194f38c9607e100
joaomanoel <joaom_oliveira@outlook.com>
2023-09-20 14:36:04 -0300

    Commit das tarefas list

---
+++ src/main/java/list/Tarefa.java
@@ -0,0 +1,19 @@
+package src.main.java.list;
+
+public class Tarefa {
+  //atributo
+  private String descricao;
+
+  public Tarefa(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  @Override
+  public String toString() {
+    return  descricao;
+  }
+}
